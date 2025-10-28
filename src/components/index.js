aef1d2d192786b19ee23d8ae95b9f618e1ce1040
nithin@zinfog.com <nithinvadakkethil18@gmail.com>
2025-10-28 15:20:56 +0530

    search bar created as common component

--- src/components/index.js
+++ src/components/index.js
@@ -40,4 +40,5 @@ export { default as ContactMadeForm } from './ContactMadeForm/ContactMadeForm';
 export { default as Anniversary } from './Home/Anniversary';
 export { default as HomeScreenSkeleton } from './Skeletons/HomeScreenSkeleton';
 export { default as EmployeeCard } from './Employee/EmployeeCard';
+export { default as SearchBar } from './Common/SearchBar';
 
